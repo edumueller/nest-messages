381577f8a7f81d377f7a148be27a3eb8495d438f
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-09-28 21:14:51 -0300

    add other route examples to request file

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return '';
+  }
+
+  @Post()
+  createMessage() {
+    return '';
+  }
+
+  @Get('/:id')
+  getMessage() {
+    return '';
+  }
+}
